49b78b03121100b9c7a0412e7e2401f5959b77e3
luckyB342 <luckyB342@users.noreply.github.com>
2020-05-20 11:27:52 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+// Just to enable the get_string to run
+#include <stdio.h>
+
+int main(void)
+string name = get_string("What is yor name?\n")
+{
+    printf("hello, %s\n", name);
+    // concatenation
+}
