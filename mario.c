d355b6527d5663fb6f228bf46e8ac12063ee73b2
luckyB342 <luckyB342@users.noreply.github.com>
2020-05-24 20:57:32 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,38 @@
+// Draws a pyramid using iteration
+
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int(void);
+
+int main(void)
+{
+    // Get height of pyramid form user
+    int h = get_positive_int();
+
+    // Draw pyramid using user's input
+    for (int i = 1; i <= h; i++)
+    {
+        for (int k = i; k < h; k++)
+        {
+              printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+// Prompt user for positive integer within one and eight
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+    return n;
+}
