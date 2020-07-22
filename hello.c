3b60f810ffbf28167027b299c2edfab63d60d7d7
luckyB342 <luckyB342@users.noreply.github.com>
2020-07-22 09:55:32 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
+// A program that gets input from user and says hello to them with their input
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
+    // Get input from user
     string name = get_string("What is your name?\n");
+
+    // Print Hello to user plugin in the user input
     printf("hello, %s\n", name);
 }
