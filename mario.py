33429dcabbfe543e71fbe5553220af42a1ee7484
luckyB342 <luckyB342@users.noreply.github.com>
2020-07-30 21:37:13 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,5 +1,6 @@
-# get input from user
-h = int(input("Height: "))
+from cs50 import get_int
+
+h = get_int("Height: ")
 
 # Ensure that users typed in a positive integer between 1 and 8, inclusive.
 while h < 1 or h > 8:
