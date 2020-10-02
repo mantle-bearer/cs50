b06616ac1fcbc4b3df895f479050d3f570d4f20f
luckyB342 <luckyB342@users.noreply.github.com>
2020-10-02 18:01:15 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -3,12 +3,14 @@
 import cs50
 from sys import argv, exit
 
+# Make sure that the correct commandline argument is used
 while len(argv) > 2 or len(argv) < 2:
     print("Usage: [house name]")
     exit(1)
 
 db = cs50.SQL("sqlite:///students.db")
 
+# Output the details from the database using the users inputed house
 rows = db.execute("SELECT first, middle, last, house, birth FROM students WHERE house= :inputu ORDER BY last ASC, first ASC", inputu=argv[1])
 for row in rows:
     if row['middle'] == None:
