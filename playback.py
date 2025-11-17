39dfac7f683dfa938277ff38a59d0851f5d5f070
Goodluck Chigozie <igbokwegoodluck8@gmail.com>
2025-11-17 05:35:24 -0800

    Initial commit

---
+++ playback.py
@@ -0,0 +1,3 @@
+usr_inpt = input("Provide a short phrase: ") #gets input from user
+plbk = usr_inpt.replace(' ', '...') # replace each space with three dots
+print(plbk) # print out the finished replace input provided by the user
